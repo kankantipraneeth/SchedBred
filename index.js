709a8ab326e0cb25b647b4eb4106cd944ed7af9f
Kankanti Praneeth <kankantipraneeth@gmail.com>
2025-04-08 11:46:05 +0530

    fixed the bug of planner.ejs i.e not navigating to planner.ejs and completed the logic of planner.ejs changed the design of the whole project remaining task: complete nodemailer code and schedule the notifications

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ app.post("/signin", (req, res) => {
     // return res.status(401).json({ message: "Invalid email or password." });
     return res.render("index.ejs", { message: errorMessage });
   }
+
+  // if (isAuthenticated) {
+  //   if (user.timetable && Object.keys(user.timetable).length === days.length) {
+  //     // If timetable already fully filled, go to planner
+  //     return res.redirect(`/planner?userId=${user.id}`);
+  //   } else {
+  //     // Otherwise, continue with timetable entry
+  //     return res.redirect(
+  //       `/timetable?step=1&username=${user.username}&email=${user.email}&userId=${user.id}`
+  //     );
+  //   }
+  // }
 });
 const days = [
   "Monday",
@@ -138,10 +150,14 @@ app.post("/save-timetable", (req, res) => {
   fs.readFile("data.json", "utf8", (err, data) => {
     if (err) return res.status(500).json({ message: "Failed to read data" });
 
-    let users = JSON.parse(data);
-    let user = users.find((u) => u.id === userId);
+    // let users = JSON.parse(data);
+    // let user = users.find((u) => u.id === userId);
 
-    if (!user) return res.status(404).json({ message: "User not found." });
+    let users = JSON.parse(fs.readFileSync("data.json", "utf8"));
+    let user = users.find((u) => u.id == userId);
+
+    if (!user)
+      return res.status(404).json({ message: `User${userId} not found.` });
 
     if (!user.timetable) user.timetable = {};
     user.timetable[currentDay] = subjects;
@@ -161,6 +177,8 @@ app.get("/planner", (req, res) => {
   userId = Number(userId);
   let timetable = {};
 
+  let saved = req.query.saved === "true";
+
   if (fs.existsSync("data.json")) {
     const users = JSON.parse(fs.readFileSync("data.json", "utf8"));
     const user = users.find((u) => u.id === userId);
@@ -175,6 +193,7 @@ app.get("/planner", (req, res) => {
     userId,
     timetable,
     lengthDays,
+    saved,
   });
 });
 
@@ -192,6 +211,10 @@ app.post("/save-requirements", (req, res) => {
   let users = JSON.parse(fs.readFileSync("data.json", "utf8"));
   let user = users.find((u) => u.id === userId);
 
+  if (!user) {
+    return res.status(404).json({ message: "User not found." });
+  }
+
   user.requirements = requirements;
   user.notificationTime = notificationTime;
 
@@ -202,6 +225,106 @@ app.post("/save-requirements", (req, res) => {
     res.json({ message: "Requirements saved Successfully!" });
   });
 });
+
+app.post("/send-mail", (req, res) => {
+  let userId = req.body.userId;
+  userId = Number(userId);
+
+  if (!fs.existsSync("data.json")) {
+    res.status(404).json({ message: "user not found" });
+  }
+
+  let users = JSON.parse(fs.readFileSync("data.json", "utf8"));
+  let user = users.find((u) => u.id === userId);
+
+  const targetMail = user.email;
+  const senderMail = "praneeth.bunny2003@gmail.com";
+
+  const date = new Date();
+  let dayIndex = date.getDay(); // returns num ranging from 0 - 6 0 for sunday and so on
+
+  let nextDayIndex = (dayIndex + 1) % 7;
+  let nextDayName = days[nextDayIndex];
+
+  let nextDayReq = user.requirements[nextDayName];
+  console.log(`Current day is: ${days[nextDayIndex - 1]}`);
+  console.log("Next Day Requirements: ", nextDayReq);
+
+  const server = http.createServer((req, res) => {
+    const auth = nodemailer.createTransport({
+      service: "gmail",
+      secure: "true",
+      port: 465,
+      auth: {
+        user: senderMail,
+        pass: "lessSecurePass",
+      },
+    });
+
+    const receiver = {
+      from: senderMail,
+      to: targetMail,
+      subject: "Notification Remainder from SchedBred",
+      test: nextDayReq,
+    };
+
+    auth.sendMail(receiver, (err, res) => {
+      if (err) throw err;
+      console.log("success!");
+      res.end();
+    });
+  });
+});
+
+// function sendMail() {
+//   let userId = req.body.userId;
+//   userId = Number(userId);
+
+//   if (!fs.existsSync(data.json)) {
+//     res.status(404).json({ message: "user not found" });
+//   }
+
+//   let users = JSON.parse(fs.readFileSync("data.json", "utf8"));
+//   let user = users.find((u) => u.id === userId);
+
+//   const targetMail = user.email;
+//   const senderMail = "praneeth.bunny2003@gmail.com";
+
+//   const date = new Date();
+//   let dayIndex = date.getDay(); // returns num ranging from 0 - 6 0 for sunday and so on
+
+//   let nextDayIndex = (dayIndex + 1) % 7;
+//   let nextDayName = days[nextDayIndex];
+
+//   let nextDayReq = user.requirements[nextDayName];
+
+//   console.log("Next Day Requirements: ", nextDayReq);
+
+//   const server = http.createServer((req, res) => {
+//     const auth = nodemailer.createTransport({
+//       service: "gmail",
+//       secure: "true",
+//       port: 465,
+//       auth: {
+//         user: senderMail,
+//         pass: "lessSecurePass",
+//       },
+//     });
+
+//     const receiver = {
+//       from: senderMail,
+//       to: targetMail,
+//       subject: "Notification Remainder from SchedBred",
+//       test: nextDayReq,
+//     };
+
+//     auth.sendMail(receiver, (err, res) => {
+//       if (err) throw err;
+//       console.log("success!");
+//       res.end();
+//     });
+//   });
+// }
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on the port ${port}`);
